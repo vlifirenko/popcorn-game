eb19b437c4fa2573665088c859d249c99acbb7b6
vlifirenko <vladislav.lifirenko@gmail.com>
2024-07-21 11:30:54 +0300

    Draw level

--- main.cpp
+++ main.cpp
@@ -11,6 +11,18 @@ HINSTANCE hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 
+enum EBrickType
+{
+   EBT_None,
+   EBT_Red,
+   EBT_Blue
+};
+
+HPEN brick_red_pen;
+HPEN brick_blue_pen;
+HBRUSH brick_red_brush;
+HBRUSH brick_blue_brush;
+
 const int GLOBAL_SCALE = 3;
 const int BRICK_WIDTH = 15;
 const int BRICK_HEIGHT = 7;
@@ -18,6 +30,24 @@ const int CELL_WIDTH = 16;
 const int CELL_HEIGHT = 8;
 const int LEVEL_X_OFFSET = 8;
 const int LEVEL_Y_OFFSET = 6;
+
+char LEVEL_01[14][12] =
+{
+   0,0,0,0,0,0,0,0,0,0,0,0,
+   1,1,1,1,1,1,1,1,1,1,1,1,
+   1,1,1,1,1,1,1,1,1,1,1,1,
+   2,2,2,2,2,2,2,2,2,2,2,2,
+   2,2,2,2,2,2,2,2,2,2,2,2,
+   1,1,1,1,1,1,1,1,1,1,1,1,
+   1,1,1,1,1,1,1,1,1,1,1,1,
+   2,2,2,2,2,2,2,2,2,2,2,2,
+   2,2,2,2,2,2,2,2,2,2,2,2,
+   0,0,0,0,0,0,0,0,0,0,0,0,
+   0,0,0,0,0,0,0,0,0,0,0,0,
+   0,0,0,0,0,0,0,0,0,0,0,0,
+   0,0,0,0,0,0,0,0,0,0,0,0,
+   0,0,0,0,0,0,0,0,0,0,0,0
+};
 
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -83,12 +113,21 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hInstance      = hInstance;
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_POPCORNGAME));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = CreateSolidBrush(RGB(0, 0, 0));
+    wcex.hbrBackground  = CreateSolidBrush(RGB(15, 63, 51));
     wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_POPCORNGAME);
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
     return RegisterClassExW(&wcex);
+}
+
+void Init()
+{
+   brick_red_pen = CreatePen(PS_SOLID, 0, RGB(255, 85, 85));
+   brick_red_brush = CreateSolidBrush(RGB(255, 85, 85));
+
+   brick_blue_pen = CreatePen(PS_SOLID, 0, RGB(85, 255, 255));
+   brick_blue_brush = CreateSolidBrush(RGB(85, 255, 255));
 }
 
 //
@@ -104,6 +143,8 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // Store instance handle in our global variable
+
+   Init();
 
    RECT window_rect;
    window_rect.left = 0;
@@ -130,39 +171,53 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    return TRUE;
 }
 
-void DrawBrick(HDC hdc, int x, int y, bool is_blue)
+void DrawBrick(HDC hdc, int x, int y, EBrickType brick_type)
 {
    HPEN pen;
    HBRUSH brush;
 
-   if (is_blue)
+   switch (brick_type)
    {
-      pen = CreatePen(PS_SOLID, 0, RGB(85, 255, 255));
-      brush = CreateSolidBrush(RGB(85, 255, 255));      
-   }
-   else
-   {
-      pen = CreatePen(PS_SOLID, 0, RGB(255, 85, 255));
-      brush = CreateSolidBrush(RGB(255, 85, 255));
+   case EBT_None:
+      return;
+
+   case EBT_Red:
+      pen = brick_red_pen;
+      brush = brick_red_brush;
+      break;
+
+   case EBT_Blue:
+      pen = brick_blue_pen;
+      brush = brick_blue_brush;
+      break;
+
+   default:
+      return;
    }
 
    SelectObject(hdc, pen);
    SelectObject(hdc, brush);
 
-   Rectangle(hdc,
+   RoundRect(hdc,
       x * GLOBAL_SCALE,
       y * GLOBAL_SCALE,
       (x + BRICK_WIDTH) * GLOBAL_SCALE,
-      (y + BRICK_HEIGHT) * GLOBAL_SCALE);
+      (y + BRICK_HEIGHT) * GLOBAL_SCALE,
+      2 * GLOBAL_SCALE, 2 * GLOBAL_SCALE);
 }
 
-void DrawFrame(HDC hdc)
+void DrawLevel(HDC hdc)
 {
    for (int i = 0; i < 14; i++)
       for (int j = 0; j < 12; j++)
       {
-         DrawBrick(hdc, LEVEL_X_OFFSET + j * CELL_WIDTH, LEVEL_Y_OFFSET + i * CELL_HEIGHT, true);
+         DrawBrick(hdc, LEVEL_X_OFFSET + j * CELL_WIDTH, LEVEL_Y_OFFSET + i * CELL_HEIGHT, (EBrickType)LEVEL_01[i][j]);
       }
+}
+
+void DrawFrame(HDC hdc)
+{
+   DrawLevel(hdc);
 }
 
 //
